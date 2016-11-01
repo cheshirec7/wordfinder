42736de858198b08252516428bcbb0969d1671cd
Eric Totten <erictotten@cox.net>
2016-11-01 01:13:23 -0700

    add auto search

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             centerModal();
         }),
         $document = $(document).ajaxStart(function () {
-            if (showSpinner)
+            // if (showSpinner)
                 $searchSpinner.addClass('is-active').show();
             $btnSubmit.attr('disabled', true);
         }).ajaxError(function (event, jqxhr, settings, thrownError) {
@@ -19,6 +19,9 @@ $(document).ready(function () {
         charcode_a = 97,
         charcode_z = 122,
         showSpinner = true,
+        searching = false,
+        timeoutID = null,
+        lastTray = '',
         lastTouchedStartTime = new Date().getTime(),
         snackbarContainer = document.querySelector('#wfToast'),
         sidebarWidth = 240,
@@ -26,7 +29,7 @@ $(document).ready(function () {
         backends = ["Go", "Node", "PHP", "Python"],
 
         $searchSpinner = $("#searchSpinner"),
-        $tray = $("#tray"),
+
         $title = $('.mdl-layout-title'),
         $resultsArea = $('#resultsArea'),
         $btnSubmit = $('#btnSubmit'),
@@ -45,88 +48,24 @@ $(document).ready(function () {
             backend = parseInt($(this).val());
             $title.text(backends[backend] + 'WordFinder');
             $resultsArea.text('');
+            lastTray = '';
+        }),
+
+        $tray = $("#tray").keyup(function (e) {
+            //event.preventDefault();
+            clearTimeout(timeoutID);
+            if (!searching) {
+                timeoutID = setTimeout(
+                    doSearch.bind(undefined, false), 250);
+            }
         }),
 
         $formSearch = $("#formSearch").submit(function (event) {
             event.preventDefault();
-
-            var $traytext = $tray.val().trim().toLowerCase();
-            if ($traytext.length == 0) {
-                $tray.val('');
+            if (searching)
                 return false;
-            }
-
-            var oInput = validateInput($traytext);
-            $tray.val(oInput.tray);
-
-            if (oInput.errmsg.length > 0) {
-                snackbarContainer.MaterialSnackbar.showSnackbar({message: oInput.errmsg});
-                return false;
-            }
-
-            $resultsArea.text('');
-            $tray.blur();
-
-            var return_type = $('input[name=resultoptions]:checked').val(),
-                startTime = new Date(),
-                url = 'http://wordfinder.erictotten.info/find';
-
-            switch (backend) {
-                case 0:
-                    url = 'http://gowordfinder.appspot.com/find';
-                    break;
-                case 1:
-                    url = 'http://nodewordfinder.herokuapp.com/find';
-                    break;
-                case 2:
-                    url = 'http://phpwordfinder.erictotten.info/find';
-                    break;
-                case 3:
-                    url = 'http://pywordfinder.appspot.com/search';
-                    break;
-            }
-
-            $.ajax({
-                type: 'GET',
-                url: url,
-                cache: false,
-                data: {tray: oInput.letters_only, sm: 0, rt: return_type, wc: oInput.wild_count}
-            }).done(function (data, textStatus, jqXHR) {
-                if (return_type == 'html') {
-                    $resultsArea.html(data);
-                    setTimeout(function () {
-                        var endTime1 = new Date(),
-                            timeDiff1 = (endTime1 - startTime) / 1000,
-                            seconds1 = Math.round((timeDiff1 % 60) * 100) / 100;
-                        $('#results_footer', $resultsArea).append('<br />Total time: ' + seconds1 + ' secs.');
-                    }, 1);
-                } else {
-                    var wordlist, total = 0,
-                        res = '<h5 id="resultsFor">Results for <span>' + oInput.tray + '</span></h5>';
-
-                    if (data.length == 0)
-                        res += '<p class="noresults">No words found.</p>';
-
-                    for (var i = 0; i < data.length; i++) {
-                        wordlist = data[i];
-                        if (wordlist.length == 0)
-                            continue;
-                        res += '<p>' + wordlist[0].length + ' Letter Words</p><div class="wordcontainer">';
-                        for (var j = 0, k = wordlist.length; j < k; j++) {
-                            res += '<div>' + wordlist[j] + '</div>';
-                            total++;
-                        }
-                        res += '</div>';
-                    }
-                    $resultsArea.html(res);
-                    setTimeout(function () {
-                        var endTime2 = new Date(),
-                            timeDiff2 = (endTime2 - startTime) / 1000,
-                            seconds2 = Math.round((timeDiff2 % 60) * 100) / 100;
-                        $resultsArea.append('<p id="results_footer">' + total + ' results in ' + seconds2 + ' secs.</p>');
-                    }, 1);
-                }
-            });
+            else
+                doSearch(true);
         }),
 
         definitionSearch1 = $document.on('click', '#resultsArea .wordcontainer div', function (e) {
@@ -198,6 +137,103 @@ $(document).ready(function () {
             $dialog.css("margin-left", (winWidth / 2) - (dWidth / 2));
         else
             $dialog.css("margin-left", $dialog.css("margin-right"));
+    }
+
+    function doSearch(findButtonClicked) {
+
+        var startTime = new Date(),
+            oInput = {},
+            return_type = 'html',
+            findurl = 'http://wordfinder.erictotten.info/find',
+            $traytext = $tray.val().trim().toLowerCase();
+
+        if (!findButtonClicked && $traytext.length < 2)
+            return false;
+
+        if ($traytext.length == 0) {
+            $tray.val('');
+            return false;
+        }
+
+        oInput = validateInput($traytext);
+        $tray.val(oInput.tray);
+
+        if (oInput.errmsg.length > 0) {
+            snackbarContainer.MaterialSnackbar.showSnackbar({message: oInput.errmsg});
+            return false;
+        }
+
+        if (lastTray == oInput.tray)
+            return;
+
+        if ( findButtonClicked ) {
+            $resultsArea.text('');
+            $tray.blur();
+        }
+
+        return_type = $('input[name=resultoptions]:checked').val();
+
+        switch (backend) {
+            case 0:
+                findurl = 'http://gowordfinder.appspot.com/find';
+                break;
+            case 1:
+                findurl = 'http://nodewordfinder.herokuapp.com/find';
+                break;
+            case 2:
+                findurl = 'http://phpwordfinder.erictotten.info/find';
+                break;
+            case 3:
+                findurl = 'http://pywordfinder.appspot.com/search';
+                break;
+        }
+
+        searching = true;
+        showSpinner = true;//findButtonClicked;
+        lastTray = oInput.tray;
+        $.ajax({
+            type: 'GET',
+            url: findurl,
+            cache: false,
+            data: {tray: oInput.letters_only, sm: 0, rt: return_type, wc: oInput.wild_count}
+        }).done(function (data) {
+            if (return_type == 'html') {
+                $resultsArea.html(data);
+                setTimeout(function () {
+                    var endTime1 = new Date(),
+                        timeDiff1 = (endTime1 - startTime) / 1000,
+                        seconds1 = Math.round((timeDiff1 % 60) * 100) / 100;
+                    $('#results_footer', $resultsArea).append('<br />Total time: ' + seconds1 + ' secs.');
+                    searching = false;
+                }, 1);
+            } else {
+                var wordlist, total = 0,
+                    res = '<h5 id="resultsFor">Results for <span>' + oInput.tray + '</span></h5>';
+
+                if (data.length == 0)
+                    res += '<p class="noresults">No words found.</p>';
+
+                for (var i = 0; i < data.length; i++) {
+                    wordlist = data[i];
+                    if (wordlist.length == 0)
+                        continue;
+                    res += '<p>' + wordlist[0].length + ' Letter Words</p><div class="wordcontainer">';
+                    for (var j = 0, k = wordlist.length; j < k; j++) {
+                        res += '<div>' + wordlist[j] + '</div>';
+                        total++;
+                    }
+                    res += '</div>';
+                }
+                $resultsArea.html(res);
+                setTimeout(function () {
+                    var endTime2 = new Date(),
+                        timeDiff2 = (endTime2 - startTime) / 1000,
+                        seconds2 = Math.round((timeDiff2 % 60) * 100) / 100;
+                    $resultsArea.append('<p id="results_footer">' + total + ' results in ' + seconds2 + ' secs.</p>');
+                    searching = false;
+                }, 1);
+            }
+        });
     }
 
     $searchSpinner.hide();
