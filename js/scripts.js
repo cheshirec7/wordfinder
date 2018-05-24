07651f7135aac445dc62b6724b690af30d30ddca
Eric Totten <erictotten@cox.net>
2018-05-24 12:27:07 -0700

    tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     "use strict";
 
-    var $window = $(window).on('resize', function () {
+    let $window = $(window).on('resize', function () {
             centerModal();
         }),
         $document = $(document).ajaxStart(function () {
             // if (showSpinner)
-                $searchSpinner.addClass('is-active').show();
+            $searchSpinner.addClass('is-active').show();
             $btnSubmit.attr('disabled', true);
         }).ajaxError(function (event, jqxhr, settings, thrownError) {
             $searchSpinner.hide().removeClass('is-active');
@@ -53,8 +53,8 @@ $(document).ready(function () {
         }),
 
         $tray = $("#tray").keyup(function (e) {
-           if (isMobile)
-               return;
+            if (isMobile)
+                return;
             clearTimeout(timeoutID);
             if (!searching) {
                 timeoutID = setTimeout(
@@ -76,7 +76,7 @@ $(document).ready(function () {
         definitionSearch2 = $document.on('touchstart', '#resultsArea .wordcontainer div', function (e) {
             lastTouchedStartTime = new Date().getTime();
         }).on('touchend', '#resultsArea .wordcontainer div', function (e) {
-            var diff = new Date().getTime() - lastTouchedStartTime;
+            let diff = new Date().getTime() - lastTouchedStartTime;
             if (diff > 350) {
                 e.stopPropagation();
                 e.preventDefault();
@@ -89,7 +89,7 @@ $(document).ready(function () {
         showSpinner = false;
         $.ajax({
             type: 'GET',
-            url: 'https://phpwordfinder.erictotten.net/define/' + word,
+            url: '//phpwordfinder.erictotten.org/define/' + word,
             dataType: 'html'
         }).done(function (data, textStatus, jqXHR) {
             $defModalBody.html(data);
@@ -99,7 +99,7 @@ $(document).ready(function () {
     }
 
     function validateInput($traytext) {
-        var charcode = 0, i, j,
+        let charcode = 0, i, j,
             result = {
                 tray: '',
                 letters_only: '',
@@ -108,7 +108,7 @@ $(document).ready(function () {
             };
 
         for (i = 0, j = $traytext.length; i < j; i++) {
-            if ($traytext[i] == '?')
+            if ($traytext[i] === '?')
                 result.wild_count++;
             else {
                 charcode = $traytext.charCodeAt(i);
@@ -128,7 +128,7 @@ $(document).ready(function () {
     }
 
     function centerModal() {
-        var $dialog = $defModal.find('.modal-dialog'),
+        let $dialog = $defModal.find('.modal-dialog'),
             winWidth = $window.width(),
             dWidth = $dialog.width();
 
@@ -143,16 +143,16 @@ $(document).ready(function () {
 
     function doSearch(findButtonClicked) {
 
-        var startTime = new Date(),
+        let startTime = new Date(),
             oInput = {},
             return_type = 'html',
-            findurl = 'http://wordfinder.erictotten.net/find',
+            findurl = '//wordfinder.erictotten.org/find',
             $traytext = $tray.val().trim().toLowerCase();
 
         if (!findButtonClicked && $traytext.length < 2)
             return false;
 
-        if ($traytext.length == 0) {
+        if ($traytext.length === 0) {
             $tray.val('');
             return false;
         }
@@ -165,10 +165,10 @@ $(document).ready(function () {
             return false;
         }
 
-        if (lastTray == oInput.tray)
+        if (lastTray === oInput.tray)
             return;
 
-        if ( findButtonClicked ) {
+        if (findButtonClicked) {
             $resultsArea.text('');
             $tray.blur();
         }
@@ -177,16 +177,16 @@ $(document).ready(function () {
 
         switch (backend) {
             case 0:
-                findurl = 'http://gowordfinder.appspot.com/find';
+                findurl = '//gowordfinder.appspot.com/find';
                 break;
             case 1:
-                findurl = 'http://nodewordfinder.herokuapp.com/find';
+                findurl = '//nodewordfinder.herokuapp.com/find';
                 break;
             case 2:
-                findurl = 'https://phpwordfinder.erictotten.net/find';
+                findurl = '//phpwordfinder.erictotten.net/find';
                 break;
             case 3:
-                findurl = 'http://pywordfinder.appspot.com/search';
+                findurl = '//pywordfinder.appspot.com/search';
                 break;
         }
 
@@ -199,28 +199,28 @@ $(document).ready(function () {
             cache: false,
             data: {tray: oInput.letters_only, sm: 0, rt: return_type, wc: oInput.wild_count}
         }).done(function (data) {
-            if (return_type == 'html') {
+            if (return_type === 'html') {
                 $resultsArea.html(data);
                 setTimeout(function () {
-                    var endTime1 = new Date(),
+                    let endTime1 = new Date(),
                         timeDiff1 = (endTime1 - startTime) / 1000,
                         seconds1 = Math.round((timeDiff1 % 60) * 100) / 100;
                     $('#results_footer', $resultsArea).append('<br />Total time: ' + seconds1 + ' secs.');
                     searching = false;
                 }, 1);
             } else {
-                var wordlist, total = 0,
+                let wordlist, total = 0,
                     res = '<h5 id="resultsFor">Results for <span>' + oInput.tray + '</span></h5>';
 
-                if (data.length == 0)
+                if (data.length === 0)
                     res += '<p class="noresults">No words found.</p>';
 
-                for (var i = 0; i < data.length; i++) {
+                for (let i = 0; i < data.length; i++) {
                     wordlist = data[i];
-                    if (wordlist.length == 0)
+                    if (wordlist.length === 0)
                         continue;
                     res += '<p>' + wordlist[0].length + ' Letter Words</p><div class="wordcontainer">';
-                    for (var j = 0, k = wordlist.length; j < k; j++) {
+                    for (let j = 0, k = wordlist.length; j < k; j++) {
                         res += '<div>' + wordlist[j] + '</div>';
                         total++;
                     }
@@ -228,7 +228,7 @@ $(document).ready(function () {
                 }
                 $resultsArea.html(res);
                 setTimeout(function () {
-                    var endTime2 = new Date(),
+                    let endTime2 = new Date(),
                         timeDiff2 = (endTime2 - startTime) / 1000,
                         seconds2 = Math.round((timeDiff2 % 60) * 100) / 100;
                     $resultsArea.append('<p id="results_footer">' + total + ' results in ' + seconds2 + ' secs.</p>');
@@ -241,7 +241,7 @@ $(document).ready(function () {
     $searchSpinner.hide();
     $tray.focus();
 
-    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
 
 });
